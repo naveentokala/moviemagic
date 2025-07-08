32627c9a027bb7a314b1b9d05262374f397e2f2e
naveentokala <228x1a05a8@khitguntur.ac.in>
2025-07-08 17:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -229,5 +229,5 @@ def debug_bookings():
     return html
 
 if __name__ == '__main__':
-   # print("🚀 Mock MovieMagic running at http://127.0.0.1:5000")
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
